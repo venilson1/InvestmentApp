dcf9cba9eafb23c3921ef2b46201469d212dc76b
venilson1 <venilson2009@hotmail.com>
2021-08-25 13:13:27 -0300

    refactor: criptoPage with new object manipulate

--- src/components/Investments.jsx
+++ src/components/Investments.jsx
@@ -1,27 +1,10 @@
-import calculatePercetage from "../helpers/calculatePercetage";
-
 export default function Investments({
-  children: investments = [],
+  children: investments
 }) {
-
-  const percetage = calculatePercetage(investments)
-  let i = 0;
 
   return (
     <div className="border-2 border-black">
-      {
-        investments.map(investment =>
-          <div key={investment.id} className="border-2 border-black m-2 p-2">
-            <ul>
-              <li className="space-x-4">
-                <span>{`${investment.month}/${investment.year}`}</span>
-                <strong>R$ {investment.value.toFixed(2)}</strong>
-                <span className="font-semibold float-right">{percetage[i++]} %</span>
-              </li>
-            </ul>
-          </div>
-        )
-      }
+      {investments}
     </div>
   )
 }
--- src/pages/CriptoPage.jsx
+++ src/pages/CriptoPage.jsx
@@ -1,25 +1,23 @@
 import Header from "../components/Header"
 import Main from "../components/Main"
-import {data, investments} from '../data/investments'
+import {investments} from '../data/investments'
 import Investments from '../components/Investments';
-import calculatePercetageGeneral from './../helpers/calculatePercetageGeneral';
 
 export default function CriptoPage() {
-
-  const criptoId = data.investments[1].id;
-  const criptoInvestments = data.reports
-  .filter(e => e.investmentId === criptoId)
-  .sort((a, b) => a.month - b.month)
-
-  console.log(investments);
-
-  const income = calculatePercetageGeneral(criptoInvestments)
 
   return (
     <div>
       <Header/>
-      <Main title={data.investments[1].description} income={income}>
-        <Investments>{criptoInvestments}</Investments>
+      <Main>
+        <Investments>
+          {
+            investments.map(({id, description}) => {
+              return (
+                <p>{id}</p>
+              )
+            })
+          }
+        </Investments>
       </Main>
     </div>
   )
